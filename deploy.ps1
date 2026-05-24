912d7dbf282b07211ceb86f5090c08c112ed2a76
Kyle <kyleperalestudio@gmail.com>
2026-05-24 22:34:28 +0800

    Deployment to home server

---
+++ deploy.ps1
@@ -0,0 +1,4 @@
+# Uncomment after updating with correct path and variables
+#Set-Location "C:\...\fintrack" <- Update with fintrack folder path
+#scp -r . <user>@192.168.x.x:/home/<user>/fintrack
+#ssh <user>@192.168.x.x "cd /home/<user>/fintrack && sudo docker compose up -d --build"
